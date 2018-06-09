380d204b8ec13d70a42923492f27402f6f31790e
jeyong <jeyong@gmail.com>
2018-06-09 12:34:38 +0900

    add seq - 1

--- Tools/speedcheck/drawintegrity.py
+++ Tools/speedcheck/drawintegrity.py
@@ -6,6 +6,7 @@ with open('../250Hz/log0011_speedchecker_info_0.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     isstart = False 
     count = 0
+    prev_seq = 0
     x = [] #sequence
     y = [] #increment
     for row in reader:
@@ -15,11 +16,12 @@ with open('../250Hz/log0011_speedchecker_info_0.csv') as csvfile:
                 isstart = True
                 count = 1
         elif isstart == True:
-            y.append(cur_seq)
+            y.append(cur_seq - (prev_seq%1000) - 1)
             x.append(count)
             count = count + 1
             if count > (250 * 60 * 30): #250Hz * 60 (sec) * 30 min
                 break
+        prev_seq = cur_seq
 
     plt.plot(x, y)
     plt.xlabel('topics count')
